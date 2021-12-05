b31324e8ef1c0a698cfe59077360d53ea6c1a625
sreyasqb <20pt33@psgtech.ac.in>
2021-12-05 20:38:16 +0530

    router done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import Homepage from "./pages/Homepage";
 import Loginpage from "./pages/Loginpage";
 import CoursePage from "./pages/CoursePage";
+import { Route, Routes,Navigate } from "react-router-dom";
 
 const App = () => {
   return (
     <div>
-    <CoursePage />
-      {/* <Homepage /> */}
-      {/* <Loginpage/> */}
+      <Routes>
+        <Route path="/" element={<Navigate to="/homepage"/>}/>
+        <Route path="/homepage" element={<Homepage/>}/>
+        <Route path="/coursepage" element={<CoursePage/>}/>
+        <Route path="/loginpage" element={<Loginpage/>}/>
+      </Routes>
     </div>
   );
 };
